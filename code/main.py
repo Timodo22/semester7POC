6aa5e308409c8c4d36ebdd541322867c77218d31
Tollie Bonker <timosteen22@gmail.com>
2026-01-12 11:12:02 +0100

    Removed lines that did nothing

--- code/main.py
+++ code/main.py
@@ -8,7 +8,6 @@ from prometheus_client import Counter, make_asgi_app
 
 app = FastAPI()
 
-# CORS Setup
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
@@ -18,19 +17,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Prometheus Metrics
+
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
 
-# WAARSCHUWING: In productie stop je NOOIT usernames in labels (cardinality explosion).
-# Voor deze POC is het echter precies wat we willen zien.
 LOGIN_COUNTER = Counter(
     'login_attempts_total', 
     'Totaal aantal inlogpogingen', 
     ['status', 'ip', 'country', 'username']
 )
 
-# Database Config
+
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = "postgres"
 DB_PASS = "postgres"
@@ -44,12 +41,10 @@ def get_db_connection():
         return conn
     except Exception as e:
         print(f"Connection Error: {e}")
-        # Geen raise, anders crasht de app als DB even down is
         return None
 
 def get_country_from_ip(ip):
     try:
-        # Lokale IP's negeren
         if ip == "127.0.0.1" or ip.startswith("192.168") or ip.startswith("10."):
             return "Local Network"
         
@@ -67,21 +62,20 @@ class LoginRequest(BaseModel):
 
 @app.post("/login")
 def login(request: Request, login_data: LoginRequest):
-    # 1. Haal ECHTE IP adres op (ook achter proxy/loadbalancer)
     forwarded = request.headers.get("X-Forwarded-For")
     if forwarded:
         client_ip = forwarded.split(",")[0]
     else:
         client_ip = request.client.host
     
-    # 2. Haal land op
+
     country = get_country_from_ip(client_ip)
 
     status = "failed"
     if login_data.password == "geheim":
         status = "success"
     
-    # 3. Update Prometheus (NU MET USERNAME!)
+
     LOGIN_COUNTER.labels(
         status=status, 
         ip=client_ip, 
@@ -89,7 +83,7 @@ def login(request: Request, login_data: LoginRequest):
         username=login_data.username
     ).inc()
 
-    # 4. Opslaan in Database
+
     conn = get_db_connection()
     if conn:
         try:
